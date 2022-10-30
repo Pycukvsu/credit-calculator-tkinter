16ac07df574d4b92a56155beb12848f9743111c2
Pycukvsu <80128381+Pycukvsu@users.noreply.github.com>
2022-10-30 18:16:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import math
 import tkinter as tk
 from tkinter import *
@@ -7,18 +8,18 @@ from tkinter.ttk import Treeview
 window = Tk()
 window.title("Кредитный калькулятор")
 window.geometry('500x400')
-messages = []
 pereplats = []
-
 
 frame = Frame(window, padx=10, pady=10)
 frame.pack(expand=True)
-
 sum = Label(frame,text="Cумма кредита:  ")
 sum.grid(row=1, column=1)
 
 period = Label(frame,text="Период кредитования:  ",)
 period.grid(row=2, column=1)
+
+proc = Label(frame,text="Процентная ставка:  ")
+proc.grid(row=3, column=1)
 
 okno_sum = Entry(frame,)
 okno_sum.grid(row=1, column=2)
@@ -26,11 +27,14 @@ okno_sum.grid(row=1, column=2)
 okno_period = Entry(frame,)
 okno_period.grid(row=2, column=2)
 
+okno_proc = Entry(frame,)
+okno_proc.grid(row=3,column=2)
+
 #okno_vid = Entry(frame,)
 #okno_vid.pack()
 
 
-def tabl():
+def tabl(arr):
 
     tabl = Tk()
     tabl.title("Платежи")
@@ -43,16 +47,17 @@ def tabl():
         tree.heading(header, text=header, anchor='center')
         tree.column(header,anchor='center')
 
-    for row in messages:
+    for row in arr:
         tree.insert("",tk.END, values=row)
 
     tree.pack(expand=tk.YES, fill=tk.BOTH)
 
 
 def anuit():
+    kred = []
     summ = int(okno_sum.get())
     period = int(okno_period.get())
-    stavka = 10
+    stavka = int(okno_proc.get())
 
     procent = int(stavka) / 100
     i = procent / 12
@@ -69,15 +74,16 @@ def anuit():
         dop.append(x+1)
         dop.append(round(a))
         dop.append(round(prockred))
-        messages.append(dop)
+        kred.append(dop)
 
-    tabl()
+    tabl(kred)
 
 
 def dif():
+    kred = []
     summ = int(okno_sum.get())
     period = int(okno_period.get())
-    stavka = 10
+    stavka = int(okno_proc.get())
 
     perepl = 0
     sum = int(summ)
@@ -93,16 +99,26 @@ def dif():
         dop.append(x+1)
         dop.append(round(p))
         dop.append(round(sum))
-        messages.append(dop)
+        kred.append(dop)
 
     pereplats.append(round(perepl))
-    tabl()
+    tabl(kred)
 
+
+def raz_pereplat():
+    dif()
+    anuit()
+    per = pereplats[1] - pereplats[0]
+    messagebox.showinfo('Разница переплат ', f'Разница между кредитами = {per}')
+
+
+calcraz_button = Button(frame, text='Посчитать разницу кредитов', command=raz_pereplat)
+calcraz_button.grid(row=5, column=1)
 
 calcdif_button = Button(frame,text='Рассчитать дифф. кредит', command=dif)
-calcdif_button.grid(row=3, column=2)
+calcdif_button.grid(row=4, column=1)
 
 calcanut_button = Button(frame, text='Рассчитать ануит. кредит',command=anuit)
-calcanut_button.grid(row=3, column=1)
+calcanut_button.grid(row=4, column=2)
 
 window.mainloop()
